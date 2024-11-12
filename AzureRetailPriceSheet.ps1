26521a2d170db3480cac90d5ad6543298629b594
Simon Hutson <simon.hutson@microsoft.com>
2024-11-12 13:44:56 +0000

    Commit

--- AzureRetailPriceSheet.ps1
+++ AzureRetailPriceSheet.ps1
@@ -1,8 +1,8 @@
-$uri = "https://prices.azure.com/api/retail/prices?currencyCode='GBP'"  # optionally add "?currencyCode='USD'"
+$uri = 'https://prices.azure.com/api/retail/prices?api-version=2023-01-01-preview&currencyCode=%27GBP%27&$filter=serviceFamily+eq+%27Compute%27+and+serviceName+eq+%27Virtual+Machines%27+and+priceType+eq+%27Consumption%27'
 $Headers = @{}
 $Items = @()
 $Count = 0
-$estTotal = 720000		# estimated total
+$estTotal = 261000 # estimated total for progress bar
 Do
 {
 	$pct = [math]::min([Math]::Truncate($Count / $estTotal * 100), 99)
@@ -16,4 +16,4 @@ Do
 write-progress -Activity "Reading Azure Retail Price Sheet" -Status "Done" -Id 1 -PercentComplete 100 -Completed
 
 # Export to CSV
-$Items | Export-CSV -Path ("$(get-date -format "Retail_yyyy_mm")" +'.csv') -Force 
+$Items | Export-CSV -Path ('AzureRetailPriceSheet_' + "$(get-date -format "yyyy_MM_dd_HH_mm")" +'.csv') -Force
